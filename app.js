a5bd6206812866a72e790c25bf84a7ba2d6209b5
Ahmet İhsan Bozacı <ahmetbozac@gmail.com>
2022-09-27 16:39:31 +0300

    Static files created

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
 
+const myLogger = (req, res, next) => {
+  console.log('Middleware Log 1');
+  next();
+};
+const myLogger2 = (req, res, next) => {
+  console.log('Middleware Log 2');
+  next();
+};
+
+//MIDDLEWARES (between of the req and res)
+app.use(express.static('public'));
+app.use(myLogger);
+app.use(myLogger2);
+
+//path directory
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.resolve(__dirname, 'temp/index.html')); 
 });
 
 const port = 3000;
