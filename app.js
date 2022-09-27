8b0c9eed8c2ebb49ad076862c6eccc93cdaf0b32
Ahmet İhsan Bozacı <ahmetbozac@gmail.com>
2022-09-27 19:51:18 +0300

    ejs uploaded

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const path = require('path');
-
 const express = require('express');
-
 const app = express();
 
+const ejs = require('ejs');
+
+const path = require('path');
+
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+/*
 const myLogger = (req, res, next) => {
   console.log('Middleware Log 1');
   next();
@@ -11,16 +16,24 @@ const myLogger = (req, res, next) => {
 const myLogger2 = (req, res, next) => {
   console.log('Middleware Log 2');
   next();
-};
+}; 
+*/
 
 //MIDDLEWARES (between of the req and res)
 app.use(express.static('public'));
-app.use(myLogger);
-app.use(myLogger2);
+// app.use(myLogger);
+// app.use(myLogger2);
 
-//path directory
+//ROUTES (path directory)
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html')); 
+  //for static >> res.sendFile(path.resolve(__dirname, 'temp/index.html')); 
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
 const port = 3000;
