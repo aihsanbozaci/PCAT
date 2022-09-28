9cdc13e97ed0fd7bc569df10b7f69879aed92a8a
Ahmet İhsan Bozacı <ahmetbozac@gmail.com>
2022-09-29 00:38:58 +0300

    file upload integration,mongoose-mongodb

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+//connect MongoDB with Mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 const ejs = require('ejs');
 
 const path = require('path');
 
+const Photo = require('./models/Photo');
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
@@ -21,13 +28,16 @@ const myLogger2 = (req, res, next) => {
 
 //MIDDLEWARES (between of the req and res)
 app.use(express.static('public'));
-// app.use(myLogger);
-// app.use(myLogger2);
+app.use(express.urlencoded({extended : true})); //response form (reading url data)
+app.use(express.json());                        //converting data to json file to read
+
 
 //ROUTES (path directory)
-app.get('/', (req, res) => {
-  //for static >> res.sendFile(path.resolve(__dirname, 'temp/index.html')); 
-  res.render('index');
+app.get('/', async(req, res) => {
+  const photos = await Photo.find({})
+  res.render('index', {
+    photos
+  });
 });
 app.get('/about', (req, res) => {
   res.render('about');
@@ -35,6 +45,13 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
   res.render('add');
 });
+app.post('/photos', async(req, res) => {
+  //console.log(req.body);
+  await Photo.create(req.body);
+  res.redirect('/');
+});
+
+
 
 const port = 3000;
 app.listen(port, () => {
